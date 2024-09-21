b7785060f37548181115e5efbe04889a520c09af
Adriano Luis da Silva <adrianoluisdasilva@gmail.com>
2024-09-21 15:49:34 -0300

    Desafio 2 - v1.1

--- node/index.js
+++ node/index.js
@@ -10,14 +10,34 @@ const config = {
 const mysql = require('mysql2')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT into people(name) values ('Wesley')`
-connection.query(sql)
-connection.end
+//const sql = `INSERT into people(name) values ('Wesley')`
+//connection.query(sql)
+//connection.end
 
+//const connection = mysql.createConnection(config)
+/*
+app.get('/', (req, res) =>{
+    res.send('<h1>Full Cycle rocks !</h1> <br> <br>')
+})
+*/
 
 app.get('/', (req, res) =>{
-    res.send('<h1>Full Cycle rocks !')
+    const query = connection.query(`select * from people`, function (err, result, fields) {
+        //if (err) throw err
+        console.log(result)
+        res.json(result)
+    })
 })
+
+connection.end
+
+/*
+    query.on('result', (row) => {
+        console.log(row.id + '-' + row.name)
+        res.send(row.id + '-' + row.name)
+    })
+*/
+
 
 app.listen(port, () => {
     console.log('Rodando na porta ' + port)
